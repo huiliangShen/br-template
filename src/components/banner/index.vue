68c488e5090125ee334f9dbd77cac87655f9a840
banruo <shl@dataqin.com>
2021-06-08 11:03:43 +0800

    first commit

---
+++ src/components/banner/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <a :href="obj.href">
+    <img :src="obj.src" width="100%" alt="图片" />
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'banner',
+  props: {
+    obj: {
+      type: Object,
+      default: () => {},
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ src/components/form/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input />
+    </div>
+    <div>
+        <button>{{obj.btnText}}</button>
+    </div>
+   
+  </template>
+  
+  <script>
+  export default {
+    name: 'form',
+    props: {
+      obj: {
+        type: Object,
+        default: () => {},
+      },
+    },
+  }
+  </script>
+  
+  <style></style>
+  
